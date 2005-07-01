c2bc3b67c121ffb1152333e268b675643f9f477f
Daniel Stone <daniel@fooishbar.org>
2005-07-01 22:43:23 +0000

    Change all misc.h and os.h references to <X11/foo.h>.

--- src/mouse.c
+++ src/mouse.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.6 2005/04/20 12:25:34 daniels Exp $ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.8 2005/06/25 21:17:02 ajax Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.79 2003/11/03 05:11:48 tsi Exp $ */
 /*
  *
