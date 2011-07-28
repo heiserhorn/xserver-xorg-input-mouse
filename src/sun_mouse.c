heiserhorn/xserver-xorg-input-mouse

commit b12fa0d5ab23237bc2ac02143739ef6861e55146

Fix Solaris issues with new ABI12 init process.
Based on BSD changes in commit a22879c6779283684fe4a61543fc95179b4f5d0b
by Alexandr Shadchin

Fix segfaults when mouse device fails to open.

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/sun_mouse.c
@@ -212,76 +212,8 @@ vuidPreInit(InputInfoPtr pInfo, const char *protocol, int flags)
 	return FALSE;
     }
 
-    pMse->protocol = protocol;
-    xf86Msg(X_CONFIG, "%s: Protocol: %s\n", pInfo->name, protocol);
-
-    /* Collect the options, and process the common options. */
-    COLLECT_INPUT_OPTIONS(pInfo, NULL);
-    xf86ProcessCommonOptions(pInfo, pInfo->options);
-
     pVuidMse->buffer = (unsigned char *)&pVuidMse->event;
     pVuidMse->strmod = xf86SetStrOption(pInfo->options, "StreamsModule", NULL);
-
-    /* Check if the device can be opened. */
-    pInfo->fd = xf86OpenSerial(pInfo->options);
-    if (pInfo->fd == -1) {
-	if (xf86GetAllowMouseOpenFail()) {
-	    xf86Msg(X_WARNING, "%s: cannot open input device\n", pInfo->name);
-	} else {
-	    xf86Msg(X_ERROR, "%s: cannot open input device\n", pInfo->name);
-	    free(pVuidMse->strmod);
-	    free(pVuidMse);
-	    free(pMse);
-	    return FALSE;
-	}
-    } else {
-	if (pVuidMse->strmod) {
-	    /* Check to see if module is already pushed */
-	    SYSCALL(i = ioctl(pInfo->fd, I_FIND, pVuidMse->strmod));
-
-	    if (i == 0) { /* Not already pushed */
-		SYSCALL(i = ioctl(pInfo->fd, I_PUSH, pVuidMse->strmod));
-		if (i < 0) {
-		    xf86Msg(X_ERROR,
-			"%s: cannot push module '%s' onto mouse device: %s\n",
-			pInfo->name, pVuidMse->strmod, strerror(errno));
-		    xf86CloseSerial(pInfo->fd);
-		    pInfo->fd = -1;
-		    free(pVuidMse->strmod);
-		    free(pVuidMse);
-		    free(pMse);
-		    return FALSE;
-		}
-	    }
-	}
-
-	buttons = xf86SetIntOption(pInfo->options, "Buttons", 0);
-	if (buttons == 0) {
-	    SYSCALL(i = ioctl(pInfo->fd, MSIOBUTTONS, &buttons));
-	    if (i == 0) {
-		pInfo->options =
-		    xf86ReplaceIntOption(pInfo->options,
-					 "Buttons", buttons);
-		xf86Msg(X_INFO, "%s: Setting Buttons option to \"%d\"\n",
-			pInfo->name, buttons);
-	    }
-	}
-
-	if (pVuidMse->strmod) {
-	    SYSCALL(i = ioctl(pInfo->fd, I_POP, pVuidMse->strmod));
-	    if (i == -1) {
-		xf86Msg(X_WARNING,
-			"%s: cannot pop module '%s' off mouse device: %s\n",
-			pInfo->name, pVuidMse->strmod, strerror(errno));
-	    }
-	}
-
-	xf86CloseSerial(pInfo->fd);
-	pInfo->fd = -1;
-    }
-
-    /* Process common mouse options (like Emulate3Buttons, etc). */
-    pMse->CommonOptions(pInfo);
 
     /* Setup the local procs. */
     pVuidMse->wrapped_device_control = pInfo->device_control;

src/mouse.c
@@ -253,6 +253,7 @@ static MouseProtocolRec mouseProtocols[] = {
     /* Misc (usually OS-specific) */
     { "SysMouse",		MSE_MISC,	mlDefaults,	PROT_SYSMOUSE },
     { "WSMouse",		MSE_MISC,	NULL,		PROT_WSMOUSE },
+    { "VUID",			MSE_MISC,	NULL,		PROT_VUID },
 
     /* end of list */
     { NULL,			MSE_NONE,	NULL,		PROT_UNKNOWN }
@@ -819,6 +820,7 @@ MousePickProtocol(InputInfoPtr pInfo, const char* device,
 
     switch (protocolID) {
         case PROT_WSMOUSE:
+        case PROT_VUID:
             if (osInfo->PreInit)
                 osInfo->PreInit(pInfo, protocol, 0);
             break;

src/mouse.h
@@ -78,6 +78,7 @@ typedef enum {
     PROT_AUTO,
     PROT_SYSMOUSE,
     PROT_WSMOUSE,
+    PROT_VUID,
     PROT_NUMPROTOS	/* This must always be last. */
 } MouseProtocolID;
 

commit 7bb5cbecbcc24f20467546f957fb95c9190514c0

sun_mouse.c: Ensure vuidMouse setup routines are called for protocol "Auto"
Fixes regression caused by commit b12fa0d5ab23 in which devices with
protocol "Auto" (generally just PS/2 devices on Solaris, since HAL
sets protocol "VUID" for USB devices already) weren't going through
vuidMousePreInit and setting up the device_control & read_input pointers
to the routines which know how to decode VUID packets.

Adds a check to vuidPreInit to ensure we don't leak memory if called
twice for the same device, just in case I missed a code path, or a
new one appears in the future.

Fixes Solaris bug 7070321: Mouse protocol "Auto" does not work in build 170

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Acked-by: Peter Hutterer <peter.hutterer@who-t.net>

src/sun_mouse.c
@@ -205,6 +205,10 @@ vuidPreInit(InputInfoPtr pInfo, const char *protocol, int flags)
     VuidMsePtr pVuidMse;
     int buttons, i;
 
+    /* Ensure we don't add the same device twice */
+    if (getVuidMsePriv(pInfo) != NULL)
+	return TRUE;
+
     pVuidMse = calloc(sizeof(VuidMseRec), 1);
     if (pVuidMse == NULL) {
 	xf86Msg(X_ERROR, "%s: cannot allocate VuidMouseRec\n", pInfo->name);
@@ -684,7 +688,9 @@ SetupAuto(InputInfoPtr pInfo, int *protoPara)
     } else if (pMse->protocolID == PROT_AUTO) {
 	pdev = xf86CheckStrOption(pInfo->options,
 		"Device", NULL);
-	solarisMouseAutoProbe(pInfo, &pproto, &pdev);
+	if ((solarisMouseAutoProbe(pInfo, &pproto, &pdev) != FALSE) &&
+	    (pproto != NULL))
+	    sunMousePreInit(pInfo, pproto, 0);
     }
     return pproto;
 }